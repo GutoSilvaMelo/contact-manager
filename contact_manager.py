ae84658420de45335941860bf681900aa92c4f49
GutoSilvaMelo <gutemberg.trindademelo@gmail.com>
2024-06-09 23:43:24 -0300

    modificações na main

--- contact_manager.py
+++ contact_manager.py
@@ -1,18 +1,22 @@
 def criar_contato(nome, telefone):
-    contato = {"NOME": nome, "TELEFONE": telefone}
+    contato = {"Nome": nome, "Telefone": telefone}
     return contato
+
 
 def adicionar_contato(lista_contatos, dicionario_contatos):
     lista_contatos.append(dicionario_contatos)
     return lista_contatos
 
+
 def remover_contato_por_telefone(lista_contatos, telefone):
     for elemento in lista_contatos:
-         if (elemento["TELEFONE"] == telefone):
-             lista_contatos.remove(elemento)
+        if elemento["Telefone"] == telefone:
+            lista_contatos.remove(elemento)
     return lista_contatos
 
-def listar_contatos(dicionario_contatos):
+
+def listar_contatos(lista_contatos):
     print("Lista de contatos:")
-    for nome, telefone in dicionario_contatos.items():
-        print(f"Nome: {nome}, Telefone: {telefone}")
+    for i in range(len(lista_contatos)):
+        contato = lista_contatos[i]
+        print(f"Nome: {contato['Nome']}, Telefone: {contato['Telefone']}")
